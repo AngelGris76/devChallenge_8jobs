a4c5d862afa452631e2203c9244f08a3e3703d7b
Sebastian Smuraglia <sebastian.smuraglia@gmail.com>
2023-12-17 11:32:55 -0300

    modified jobslist to use JobResume

--- src/components/JobsList.jsx
+++ src/components/JobsList.jsx
@@ -1,6 +1,33 @@
+import JobResume from './JobResume';
+
 const JobsList = ({ jobs }) => {
 	console.log(jobs);
-	return <ul></ul>;
+
+	const renderedJobs = jobs.map((job) => (
+		<JobResume
+			key={job.id}
+			company={job.company.display_name}
+			location={job.location.display_name}
+			created={job.created}
+			contractType={job.contract_time}
+			title={job.title}
+			description={job.description}
+		/>
+	));
+
+	return (
+		<ul>
+			{renderedJobs}
+			{/*
+			<JobResume
+				company={job.company.display_name}
+				location={job.location}
+				created={job.created}
+				contractType={job.contract}
+			/>
+	*/}
+		</ul>
+	);
 };
 
 export default JobsList;
