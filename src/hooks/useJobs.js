aa625078750d47711d6df08b3995969e7ed28fa1
Sebastian Smuraglia <sebastian.smuraglia@gmail.com>
2023-12-17 11:29:37 -0300

    hook useJobs upgrade

--- src/hooks/useJobs.js
+++ src/hooks/useJobs.js
@@ -1,5 +1,6 @@
 import { useEffect, useState } from 'react';
 import getJobs from '../services/getJobs';
+import { AbortError } from '../myErrors';
 
 const useJobs = ({ page, location, keyWord, fullTime }) => {
 	const [jobs, setJobs] = useState({
@@ -14,19 +15,30 @@ const useJobs = ({ page, location, keyWord, fullTime }) => {
 
 	useEffect(() => {
 		if (!jobs.searching) return;
+
+		const jobsController = new AbortController();
+
 		getJobs({
 			page,
 			location,
 			keyWord,
 			fullTime,
+			signal: jobsController.signal,
 		})
 			.then((result) => {
-				console.log('salgo de la promesa con API', result);
-				setJobs((prev) => ({ ...prev, searching: false }));
+				const totalPages = Math.ceil(result.count / 5);
+				const data = result.results;
+				setJobs({ totalPages, data, searching: false });
 			})
 			.catch((err) => {
-				console.log(err);
+				if (err instanceof AbortError)
+					return console.log('react strict mode cancel fetch');
+				console.log(err.message);
 			});
+
+		return () => {
+			jobsController.abort();
+		};
 	}, [page, location, keyWord, fullTime, jobs.searching]);
 
 	return { jobs, refreshJobs };
