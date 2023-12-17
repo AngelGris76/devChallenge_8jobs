a91a805441b4ed14a3806a9915ecccb48e785d58
Sebastian Smuraglia <sebastian.smuraglia@gmail.com>
2023-12-17 11:31:07 -0300

    functional getJobs services

--- src/services/getJobs.js
+++ src/services/getJobs.js
@@ -1,15 +1,25 @@
+import { AbortError } from '../myErrors';
+
 const BASE_API_URL = 'https://api.adzuna.com/v1/api/jobs/us/search/';
 const APP_ID = import.meta.env.VITE_APP_ID;
 const APP_KEY = import.meta.env.VITE_APP_KEY;
 
-const getJobs = ({ page, location, keyWord, fullTime }) => {
+const getJobs = async ({ page, location, keyWord, fullTime, signal }) => {
 	const hasLocation = !location ? '' : `&where=${location}`;
 	const hasKeyWord = !keyWord ? '' : `&what=${keyWord}`;
 	const isFullTime = !fullTime ? '' : `&full_time=1`;
 
-	const API_URL = `${BASE_API_URL}${page.toString()}?app_id=${APP_ID}&app_key=${APP_KEY}${hasLocation}${hasKeyWord}${isFullTime}`;
+	const API_URL = `${BASE_API_URL}${page.toString()}?app_id=${APP_ID}&app_key=${APP_KEY}&results_per_page=5${hasLocation}${hasKeyWord}${isFullTime}`;
 
-	console.log(API_URL);
+	try {
+		const response = await fetch(API_URL, { signal });
+		const data = await response.json();
+
+		return data;
+	} catch (err) {
+		if (err.name === 'AbortError') throw new AbortError('fetch cancelado');
+		throw new Error('otro tipo de error');
+	}
 };
 
 export default getJobs;
