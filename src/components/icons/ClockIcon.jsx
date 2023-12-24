8e7ca395710f0b5fe2bbab9cdde6523879b46641
Sebastian Smuraglia <sebastian.smuraglia@gmail.com>
2023-12-24 14:07:57 -0300

    add missing icons

---
+++ src/components/icons/ClockIcon.jsx
@@ -0,0 +1,30 @@
+const ClockIcon = ({ width, height, ...props }) => {
+	return (
+		<svg
+			width={width}
+			height={height}
+			viewBox='0 0 24 24'
+			fill='none'
+			xmlns='http://www.w3.org/2000/svg'
+			{...props}
+		>
+			<path fill='#fff' d='M0 0h24v24H0z' />
+			<circle
+				cx={12}
+				cy={12}
+				r={9}
+				stroke='currentColor'
+				strokeLinecap='round'
+				strokeLinejoin='round'
+			/>
+			<path
+				d='M12 6v6l4.5 4.5'
+				stroke='currentColor'
+				strokeLinecap='round'
+				strokeLinejoin='round'
+			/>
+		</svg>
+	);
+};
+
+export default ClockIcon;
---
+++ src/components/icons/PortfolioIcon.jsx
@@ -0,0 +1,22 @@
+const PortfolioIcon = ({ width, height, ...props }) => {
+	return (
+		<svg
+			width={width}
+			height={height}
+			viewBox='0 0 32 32'
+			xmlns='http://www.w3.org/2000/svg'
+			{...props}
+		>
+			<path d='M28 10h-6V6a2 2 0 0 0-2-2h-8a2 2 0 0 0-2 2v4H4a2 2 0 0 0-2 2v14a2 2 0 0 0 2 2h24a2 2 0 0 0 2-2V12a2 2 0 0 0-2-2ZM12 6h8v4h-8ZM4 26V12h24v14Z' />
+			<path
+				data-name='&lt;Transparent Rectangle&gt;'
+				style={{
+					fill: 'none',
+				}}
+				d='M0 0h32v32H0z'
+			/>
+		</svg>
+	);
+};
+
+export default PortfolioIcon;
