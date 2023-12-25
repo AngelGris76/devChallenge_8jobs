9a5e38dd0fb0ab7c86d78382d48d5cecb2bbd534
Sebastian Smuraglia <sebastian.smuraglia@gmail.com>
2023-12-25 16:57:29 -0300

    minimal correction on fill in portfolio icon

--- src/components/icons/PortfolioIcon.jsx
+++ src/components/icons/PortfolioIcon.jsx
@@ -7,7 +7,11 @@ const PortfolioIcon = ({ width, height, ...props }) => {
 			xmlns='http://www.w3.org/2000/svg'
 			{...props}
 		>
-			<path d='M28 10h-6V6a2 2 0 0 0-2-2h-8a2 2 0 0 0-2 2v4H4a2 2 0 0 0-2 2v14a2 2 0 0 0 2 2h24a2 2 0 0 0 2-2V12a2 2 0 0 0-2-2ZM12 6h8v4h-8ZM4 26V12h24v14Z' />
+			<path
+				d='M28 10h-6V6a2 2 0 0 0-2-2h-8a2 2 0 0 0-2 2v4H4a2 2 0 0 0-2 2v14a2 2 0 0 0 2 2h24a2 2 0 0 0 2-2V12a2 2 0 0 0-2-2ZM12 6h8v4h-8ZM4 26V12h24v14Z'
+				stroke='currentColor'
+				fill='none'
+			/>
 			<path
 				data-name='&lt;Transparent Rectangle&gt;'
 				style={{
