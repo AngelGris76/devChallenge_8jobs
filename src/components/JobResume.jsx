5808b8874dd89f330cafb80ea47c696b4620eea9
Sebastian Smuraglia <sebastian.smuraglia@gmail.com>
2023-12-30 15:48:52 -0300

    add style for JobResume

--- src/components/JobResume.jsx
+++ src/components/JobResume.jsx
@@ -1,6 +1,7 @@
 import { Link, useLocation } from 'react-router-dom';
 import LocationIcon from './icons/LocationIcon';
 import ClockIcon from './icons/ClockIcon';
+import style from './JobResume.module.css';
 
 const JobResume = ({
 	company,
@@ -15,23 +16,35 @@ const JobResume = ({
 
 	return (
 		<li>
-			<Link to='/Description' state={{ description, urlLocation }}>
-				<div>
-					<img src='' alt={`${company} logo`} />
+			<Link
+				className={style.listLink}
+				to='/Description'
+				state={{
+					company,
+					location,
+					created,
+					contractType,
+					title,
+					description,
+					urlLocation,
+				}}
+			>
+				<div className={style.logoContainer}>
+					<img className={style.logImg} src='' alt={`${company} logo`} />
 				</div>
-				<div>
-					<span>{company}</span>
-					<span>{title}</span>
-					{isFullTime && <span>{contractType}</span>}
+				<div className={style.jobText}>
+					<span className={style.jobCompany}>{company}</span>
+					<span className={style.jobPosition}>{title}</span>
+					{isFullTime && <span className={style.jobContract}>Full time</span>}
 				</div>
-				<div>
-					<span>
+				<div className={style.jobPost}>
+					<span className={style.jobLocation}>
 						<LocationIcon width='24px' height='24px' />
-						{location}
+						{location.substr(0, location.indexOf(','))}
 					</span>
-					<span>
+					<span className={style.jobCreated}>
 						<ClockIcon width='24px' height='24px' />
-						{created}
+						{created.substr(0, created.indexOf('T'))}
 					</span>
 				</div>
 			</Link>
